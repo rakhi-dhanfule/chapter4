921e0069747bb60816c7b637420b5042576740df
rakhi-dhanfule <rakhidhanfule@gmail.com>
2019-12-16 11:48:31 +0530

    chapter 4 practice

--- conditional operators 4E/CONDITIONAL OPERATORS 4E/main.c
+++ conditional operators 4E/CONDITIONAL OPERATORS 4E/main.c
@@ -15,12 +15,10 @@
 #include<stdio.h>
 int main()
 {
-    int ch;
+    char ch;
     printf("enter the character:");
     scanf("%c",&ch);
     
-    {
-        ((((ch>=0)&&(ch<=47))||((ch>=58)&&(ch<=64))||((ch>=91)&&(ch<=96))||((ch>=123)&&(ch<=127)))?
-    printf("it is Special Symbol\n"):printf("it is not a special symbol")
-                return 0;
+   ((((ch>=0)&&(ch<=47))||((ch>=58)&&(ch<=64))||((ch>=91)&&(ch<=96))||((ch>=123)&&(ch<=127)))?printf("it is Special Symbol\n"):printf("it is not a special symbol");
+    return 0;
     }
